8060ad4d19b9b20b23137b3eb5282e2166979b76
Rudi <r.woerndle@student.tugraz.at>
2015-06-10 21:50:25 +0200

    CM,RW init tests

--- web/js/tests.js
+++ web/js/tests.js
@@ -7,14 +7,27 @@ QUnit.test( "hello test", function( assert ) {
 
 
 //-----------------------------------------------------------------------------
-// async test example 
-/*
-QUnit.test( "assert.async() test", function( assert ) {
-  var done = assert.async();
-  initTimeTable(2);
-  setTimeout(function() {
-    assert.equal( done_val, 3, "Passed!" );
-    done();
-  }, 1000);
+QUnit.test( "init test", function( assert ) {
+  assert.equal( options.timeout, 5000, "timeout ok" );
+  assert.equal( $('#start_btn').text(), "Start", "start button" );
+  assert.equal( $('#reset_btn').text(), "Reset", "reset button" );
+  assert.equal( $('#footer_txt').text(), "Oeffitrack Mobile Logtool", "footer text" );
+  assert.ok( $('#map-canvas'), "map_canvas" );
+  assert.equal( routeid, 2, "routeid" );
 });
-*/
+
+
+//-----------------------------------------------------------------------------
+QUnit.test( "load map", function( assert ) {
+  var done = assert.async();
+  setTimeout(function() {
+    assert.ok( map, "google maps object" );
+    assert.equal( map.zoom, 13, "map zoom" );
+    assert.equal( path.strokeColor, "#0000FF", "path.strokeColor" );
+    assert.equal( Math.round(map.center.A), 47, "map center A" );
+    assert.equal( Math.round(map.center.F), 16, "map center F" ); 
+    assert.equal( $('#route_name').text(), "Nestelbach-Großwilfersdorf", "route name headline" );    
+    done();
+  }, 1500);
+});
+
